61438ade5b8baa33d04fecf47dd1535b66a6e449
pitang1965 <pitang1965@gmail.com>
2022-02-09 20:00:43 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-d7ldhb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-d7ldhb)
---
+++ main.js
@@ -0,0 +1,8 @@
+import './style.css';
+import './src/foo.css';
+
+document.querySelector('#app').innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <h2 class='bar'>test</h2>
+`;
